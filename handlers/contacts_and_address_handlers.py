846fb1745607e3773bd465bbf92801075a5c33bc
pyadminrus <zh.vitaliy92@gmail.com>
2023-09-08 17:16:58 +0300

    Добавлен пост "Контакты и адрес"

---
+++ handlers/contacts_and_address_handlers.py
@@ -0,0 +1,15 @@
+from aiogram import types
+
+from messages.user_messages import contacts_and_address_text
+from system.dispatcher import dp, bot
+
+
+@dp.callback_query_handler(lambda c: c.data == 'contacts_and_address')
+async def contacts_and_address_handler(callback_query: types.CallbackQuery):
+    """Текущие акции"""
+    await bot.send_message(callback_query.from_user.id, contacts_and_address_text, parse_mode=types.ParseMode.HTML, disable_web_page_preview=True)
+
+
+def register_contacts_and_address_handler():
+    """Регистрируем handlers для 'Контакты и адрес'"""
+    dp.register_message_handler(contacts_and_address_handler)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiogram import executor
 
+from handlers.contacts_and_address_handlers import register_contacts_and_address_handler
 from handlers.current_promotions_handlers import register_current_promotions_handler
 from handlers.user_handlers import greeting_handler
 from system.dispatcher import dp
@@ -10,6 +11,7 @@ def main() -> None:
     executor.start_polling(dp, skip_updates=True)
     greeting_handler()
     register_current_promotions_handler()  # Текущие акции
+    register_contacts_and_address_handler()  # Контакты и адрес
 
 
 if __name__ == '__main__':
