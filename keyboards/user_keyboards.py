c89efa6ea8dbef57a525f634e2987c71e657ec83
vitaliyzh92 <zh.vitaliy92@yandex.ru>
2023-09-14 21:04:22 +0300

    Добавлены эмодзи для клавиатуры

--- keyboards/user_keyboards.py
+++ keyboards/user_keyboards.py
@@ -1,18 +1,16 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
 def greeting_keyboards():
     """Клавиатуры поста приветствия 👋"""
     keyboards_greeting = InlineKeyboardMarkup()
-    ask_anonymous_question = InlineKeyboardButton(text='Задать анонимный вопрос',
+    ask_anonymous_question = InlineKeyboardButton(text='🕵️ Задать анонимный вопрос',
                                                   callback_data='ask_anonymous_question')
-    sign_up = InlineKeyboardButton(text='Записаться',
+    sign_up = InlineKeyboardButton(text='📝 Записаться',
                                    callback_data='sign_up')
-    contacts_and_address = InlineKeyboardButton(text='Контакты и адрес',
+    contacts_and_address = InlineKeyboardButton(text='📞 Контакты и адрес',
                                                 callback_data='contacts_and_address')
-    contact_the_operator = InlineKeyboardButton(text='Связаться с оператором',
-                                                callback_data='contact_the_operator')
-    current_promotions = InlineKeyboardButton(text='Текущие акции',
+    contact_the_operator = InlineKeyboardButton(text='👷 Связаться с оператором', url="https://t.me/pk_alina")
+    current_promotions = InlineKeyboardButton(text='🎉 Текущие акции',
                                               callback_data='current_promotions')
 
     keyboards_greeting.row(ask_anonymous_question)  # Задать анонимный вопрос
@@ -20,7 +18,6 @@ def greeting_keyboards():
     keyboards_greeting.row(contacts_and_address, current_promotions)  # Контакты и адрес, Текущие акции
     keyboards_greeting.row(contact_the_operator)  # Связаться с оператором
     return keyboards_greeting
-
 
 if __name__ == '__main__':
     greeting_keyboards()
